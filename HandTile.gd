5a6c742ec114d40d3bb8ff61dd6c51e445219e43
Cryonyx <ciechanowski.daniel@gmail.com>
2021-11-14 21:35:43 -0800

    finalizing hand desplay, tomororw to work on placement logic and additional tokens.

--- HandTile.gd
+++ HandTile.gd
@@ -5,7 +5,10 @@ extends Sprite
 # var a = 2
 # var b = "text"
 var tileData = Vector3(0,0,0)
+var shown = false
 var destroy = false
+var mouseOver = false
+var handID = 0
 onready var Tween = get_node("Tween")
 
 # Called when the node enters the scene tree for the first time.
@@ -13,16 +16,24 @@ func _ready():
 	region_rect.position.x = tileData[0]*20
 	flip_h = false if tileData[1] == 0 else true
 	flip_v = false if tileData[2] == 0 else true
+	set_process_input(true)
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	pass
+
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_LEFT and event.pressed and mouseOver:
+			get_node("/root/Map").grabTile(handID)
+	pass
 
 func showHand():
 	Tween.interpolate_property(self, "scale", Vector2(0.3, 0),Vector2(0.5,0.5), 0.3)
 	Tween.interpolate_property(get_parent(), "position", Vector2(113,970), get_parent().position, 0.3)
 	Tween.start()
+	shown = true
 	pass
 
 func hideHand():
@@ -30,6 +41,7 @@ func hideHand():
 	Tween.interpolate_property(self, "scale", Vector2(0.5, 0.5),Vector2(0.3,0), 0.3)
 	Tween.interpolate_property(get_parent(), "position", get_parent().position,Vector2(113,970), 0.3)
 	Tween.start()
+	shown = false
 	destroy = true
 	pass
 
@@ -38,4 +50,14 @@ func _on_Tween_tween_completed(object, key):
 		get_parent().queue_free()
 	else:
 		$AnimationPlayer.play()
+	pass # Replace with function body.
+
+
+func _on_Area2D_mouse_entered():
+	mouseOver = true
+	pass # Replace with function body.
+
+
+func _on_Area2D_mouse_exited():
+	mouseOver = false
 	pass # Replace with function body.
--- Map.gd
+++ Map.gd
@@ -79,39 +79,51 @@ var rng = RandomNumberGenerator.new()
 var handTile = preload("res://HandTileTemplate.tscn")
 var hand = []
 var handShowing = false
+var curTile = -1
+var holding = false
 var tileUntilMove = 3
+var mousePos = Vector2(0,0)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	rng.randomize()
-	for i in 10:
+	for i in 5:
 		var randomTile = Tiles[Tiles.keys()[rng.randi()%Tiles.size()]]
 		var randomRotation = rng.randi()%randomTile.size() if randomTile.size()>1 else 0
 		hand.append(randomTile[randomRotation])
-	#print("Starting generation");
-	#for x in range(-1,15):
-	#	for y in range(ceil(x/2)-x,ceil(x/2)-x+8):
-	#		var randomTile = Tiles[Tiles.keys()[rng.randi()%Tiles.size()]]
-	#		var randomRotation = rng.randi()%randomTile.size() if randomTile.size()>1 else 0
-	#		randomTile = randomTile[randomRotation]
-	#		$HexMapNode/HexMap.set_cell(x,y,randomTile[0],randomTile[1],randomTile[2])
-			#print("%d, %d - %d" % [x,y,randomTile[0]])
-	var busPos = $HexMapNode/Bus.position
-	var busHex = worldToMap(busPos)
-	var curHexIO = getHexID(busHex)
-	var destHex = busHex+(Directions[3])
-	var destHexID = getHexID(busHex+Directions[3])
-	var destHexCenter = mapToWorld(destHex)
-	$HexMapNode/Bus.moveTo(mapToWorld(destHex))
-	destHex+=Directions[3]
-	$HexMapNode/Bus.moveTo(mapToWorld(destHex))
-	destHex+=Directions[2]
-	$HexMapNode/Bus.moveTo(mapToWorld(destHex))
+	$HexMapNode/GhostTile.visible = false
+	#var busPos = $HexMapNode/Bus.position
+	#var busHex = worldToMap(busPos)
+	#var curHexIO = getHexID(busHex)
+	#var destHex = busHex+(Directions[3])
+	#var destHexID = getHexID(busHex+Directions[3])
+	#var destHexCenter = mapToWorld(destHex)
+	#$HexMapNode/Bus.moveTo(mapToWorld(destHex))
+	#destHex+=Directions[3]
+	#$HexMapNode/Bus.moveTo(mapToWorld(destHex))
+	#destHex+=Directions[2]
+	#$HexMapNode/Bus.moveTo(mapToWorld(destHex))
 	pass
 
 
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_LEFT and event.pressed and holding and $Hand.get_children().size() == 0:
+			#print("Mouse Button pressed (Left)")
+			placeTile(curTile,worldToMap(mousePos))
+
+	pass
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	mousePos = get_viewport().get_mouse_position()
+	if $HexMapNode/GhostTile.visible == true:
+		$HexMapNode/GhostTile.position = mapToWorld(worldToMap(mousePos))
+		if $HexMapNode/HexMap.get_cellv(worldToMap(mousePos)) != -1:
+			$HexMapNode/GhostTile.modulate = Color(1.0,0.4,0.4)
+		else:
+			$HexMapNode/GhostTile.modulate = Color(1.0,1.0,1.0)
+
 	var Map = get_node("HexMapNode")
 	if Input.is_action_pressed("ui_up"):
 		Map.position += Vector2(0,5)
@@ -129,13 +141,20 @@ func _on_TextureRect_pressed():
 			i.get_node("HandTile").hideHand()
 		handShowing = !handShowing
 	else:
+		var halfWindowSize = get_viewport().size/2
+		rng.randomize()
 		for i in hand.size():
 			var tempHandTile = handTile.instance()
-			tempHandTile.position = Vector2(500.0+(i*20*8),500.0)
+			var angle = i*2.4+(rng.randf_range(-0.3,0.3)/((i*2.4)+1))
+			var dist = 40+(sqrt(i)*60)+((rng.randi()%10)-5)
+			tempHandTile.position = Vector2((sin(angle)*dist)+halfWindowSize.x,(cos(angle)*dist)+halfWindowSize.y)
 			tempHandTile.get_node("HandTile").tileData = hand[i]
+			tempHandTile.get_node("HandTile").handID = i
 			$Hand.add_child(tempHandTile, true)
 			tempHandTile.get_node("HandTile").showHand()
 		handShowing = !handShowing
+		holding = false
+		$HexMapNode/GhostTile.visible = false
 	pass # Replace with function body.
 
 func getHexID(pos)->Vector3:
@@ -144,8 +163,25 @@ func getHexID(pos)->Vector3:
 func worldToMap(worldPos)->Vector2:
 	return $HexMapNode/HexMap.world_to_map(worldPos/8)
 
-func mapToWorld(mapPos)->Vector2:
-	return ($HexMapNode/HexMap.map_to_world(mapPos)*8)+Vector2(19*4,19*4)-Vector2(4,0)
+func mapToWorld(mapPos, offset=false)->Vector2:
+	var offsetVec = Vector2(-4,0)+Vector2(19*4,19*4) if offset else Vector2(0,0)
+	return ($HexMapNode/HexMap.map_to_world(mapPos)*8)+offsetVec
 
 func placeTile(handID, mapPos):
+	$HexMapNode/HexMap.set_cellv(mapPos,hand[handID][0],hand[handID][1],hand[handID][2])
+	hand.remove(handID)
+	$HexMapNode/GhostTile.visible = false
+	curTile = -1
+	holding = false
 	pass
+
+func grabTile(tile):
+	_on_TextureRect_pressed()
+	curTile = tile
+	$HexMapNode/GhostTile.visible = true
+	$HexMapNode/GhostTile.region_rect.position.x = hand[tile][0]*20
+	$HexMapNode/GhostTile.flip_h = hand[tile][1]
+	$HexMapNode/GhostTile.flip_v = hand[tile][2]
+	holding = true
+	pass
+	
